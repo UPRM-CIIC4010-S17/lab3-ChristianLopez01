d65c7f8d4df7579a730141126d6ef644e8529726
4010c7 <4010c7@guanabana.ece.uprm.edu>
2017-02-08 12:17:13 -0400

    We change the Jframe, the size and location of the window and we activated the setDefaultCloseOperation.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Christian Lopez!");
+		JFrame myFrame = new JFrame("Christian Lopez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(800, 400);
 		myFrame.setSize(200, 200);
